e3f67329805e53982a10d43ee7af19da585c78ec
CuriousPhysicist <andrewhampson6@gmail.com>
2021-02-06 13:48:17 +0000

    CRUD actions for heros in place

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -5,12 +5,13 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { MessageService } from './message.service';
 
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+
   private heroesUrl = 'api/heroes'; // URL to web api
 
   constructor(
@@ -37,6 +38,36 @@ export class HeroService {
         tap(_ => this.log(`getHero id=${id}`)),
         catchError(this.handleError<Hero>(`getHero id=${id}`))
       );
+  }
+
+  /** PUT update the hero on the server */
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions)
+               .pipe(
+                 tap(_ => this.log(`updated hero id=${hero.id}`)),
+                 catchError(this.handleError<any>('updateHero'))
+               );
+  }
+
+  /** POST: add a new hero to the server */
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions)
+               .pipe(
+                 tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+                   catchError(this.handleError<Hero>('addHero'))
+               );
+  }
+
+  /** DELETE: delete the hero from the server */
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions)
+               .pipe(
+                 tap(_ => this.log(`deleted hero id=${id}`)),
+                 catchError(this.handleError<Hero>('deleteHero'))
+               );
   }
 
   // Log a HeroService message with the MessageService
